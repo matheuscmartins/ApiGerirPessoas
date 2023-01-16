6e6b70a2ac38c7e5631bddc0c3df7bb4313277f1
Matheus Cruz Martins <117648175+matheuscmartins@users.noreply.github.com>
2023-01-15 23:37:29 -0300

    Projeto criado

---
+++ src/main/java/br/com/homeoffice/apigerirpessoas/ApigerirpessoasApplication.java
@@ -0,0 +1,13 @@
+package br.com.homeoffice.apigerirpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigerirpessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigerirpessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/homeoffice/apigerirpessoas/ApigerirpessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.homeoffice.apigerirpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigerirpessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
