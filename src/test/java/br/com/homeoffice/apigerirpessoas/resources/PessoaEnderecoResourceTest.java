f0b880511657ad1377c2d417cab3bcc8a508595b
Matheus Cruz Martins <117648175+matheuscmartins@users.noreply.github.com>
2023-01-17 18:43:28 -0300

    Criado as clases de teste no package resources

---
+++ src/test/java/br/com/homeoffice/apigerirpessoas/resources/PessoaEnderecoResourceTest.java
@@ -0,0 +1,37 @@
+package br.com.homeoffice.apigerirpessoas.resources;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PessoaEnderecoResourceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findByPessoaNome() {
+    }
+}
---
+++ src/test/java/br/com/homeoffice/apigerirpessoas/resources/PessoaResourceTest.java
@@ -0,0 +1,33 @@
+package br.com.homeoffice.apigerirpessoas.resources;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PessoaResourceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
