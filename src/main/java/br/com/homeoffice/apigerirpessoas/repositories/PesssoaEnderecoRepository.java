1fa40af38858c2281115777d38e9bc9a62f6e462
Matheus Cruz Martins <117648175+matheuscmartins@users.noreply.github.com>
2023-01-17 07:42:51 -0300

    Criado classes PessoaEnderecoRepository

--- src/main/java/br/com/homeoffice/apigerirpessoas/repositories/PesssoaEnderecoRepository.java
+++ src/main/java/br/com/homeoffice/apigerirpessoas/repositories/PesssoaEnderecoRepository.java
@@ -1,10 +1,16 @@
 package br.com.homeoffice.apigerirpessoas.repositories;
 
 
+
 import br.com.homeoffice.apigerirpessoas.domain.PessoaEndereco;
+import br.com.homeoffice.apigerirpessoas.domain.dto.PessoaEnderecoDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PesssoaEnderecoRepository extends JpaRepository<PessoaEndereco, Long> {
+
+    List<PessoaEndereco> findByIdPessoa(PessoaEnderecoDTO pessoaId);
 }
